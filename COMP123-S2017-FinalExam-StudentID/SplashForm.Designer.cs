8d313a356f2f2cb74f5831d8f21b1f1fc03f1c19
300796405 <300796405@PR-A315-23>
2017-08-17 10:59:16 -0400

    Started Splash From with progress bar

--- COMP123-S2017-FinalExam-StudentID/SplashForm.Designer.cs
+++ COMP123-S2017-FinalExam-StudentID/SplashForm.Designer.cs
@@ -28,63 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.WelcomeTextBox = new System.Windows.Forms.TextBox();
-            this.GetReadyTextBox = new System.Windows.Forms.TextBox();
-            this.SplashFormTimer = new System.Windows.Forms.Timer(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashForm));
+            this.splashImage = new System.Windows.Forms.PictureBox();
+            this.splashProgressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splashImage)).BeginInit();
             this.SuspendLayout();
             // 
-            // WelcomeTextBox
+            // splashImage
             // 
-            this.WelcomeTextBox.BackColor = System.Drawing.Color.Black;
-            this.WelcomeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.WelcomeTextBox.ForeColor = System.Drawing.Color.White;
-            this.WelcomeTextBox.Location = new System.Drawing.Point(12, 73);
-            this.WelcomeTextBox.Name = "WelcomeTextBox";
-            this.WelcomeTextBox.Size = new System.Drawing.Size(576, 38);
-            this.WelcomeTextBox.TabIndex = 0;
-            this.WelcomeTextBox.Text = "Welcome, to Pick Highest Card!";
-            this.WelcomeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.splashImage.Image = ((System.Drawing.Image)(resources.GetObject("splashImage.Image")));
+            this.splashImage.Location = new System.Drawing.Point(0, -1);
+            this.splashImage.Name = "splashImage";
+            this.splashImage.Size = new System.Drawing.Size(584, 420);
+            this.splashImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.splashImage.TabIndex = 0;
+            this.splashImage.TabStop = false;
             // 
-            // GetReadyTextBox
+            // splashProgressBar
             // 
-            this.GetReadyTextBox.BackColor = System.Drawing.Color.Black;
-            this.GetReadyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.GetReadyTextBox.ForeColor = System.Drawing.Color.White;
-            this.GetReadyTextBox.Location = new System.Drawing.Point(12, 228);
-            this.GetReadyTextBox.Name = "GetReadyTextBox";
-            this.GetReadyTextBox.Size = new System.Drawing.Size(576, 38);
-            this.GetReadyTextBox.TabIndex = 1;
-            this.GetReadyTextBox.Text = "Get Ready to Start!";
-            this.GetReadyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // SplashFormTimer
-            // 
-            this.SplashFormTimer.Enabled = true;
-            this.SplashFormTimer.Interval = 3000;
-            this.SplashFormTimer.Tick += new System.EventHandler(this.SplashFormTimer_Tick);
+            this.splashProgressBar.Location = new System.Drawing.Point(0, 425);
+            this.splashProgressBar.Name = "splashProgressBar";
+            this.splashProgressBar.Size = new System.Drawing.Size(584, 36);
+            this.splashProgressBar.TabIndex = 1;
             // 
             // SplashForm
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(600, 500);
-            this.Controls.Add(this.GetReadyTextBox);
-            this.Controls.Add(this.WelcomeTextBox);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.ControlBox = false;
+            this.Controls.Add(this.splashProgressBar);
+            this.Controls.Add(this.splashImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SplashForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SplashForm";
+            this.Text = "Splash Form";
+            ((System.ComponentModel.ISupportInitialize)(this.splashImage)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox WelcomeTextBox;
-        private System.Windows.Forms.TextBox GetReadyTextBox;
-        private System.Windows.Forms.Timer SplashFormTimer;
+        private System.Windows.Forms.PictureBox splashImage;
+        private System.Windows.Forms.ProgressBar splashProgressBar;
     }
 }
