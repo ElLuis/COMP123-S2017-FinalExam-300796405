ddcc093b6771eb1262f79966c0afabe3715adf71
300796405 <300796405@PR-A315-23>
2017-08-17 11:42:49 -0400

    Added all the properties of the ScoreBoard class

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    public class ScoreBoard
+    {
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+
+        public TextBox FinalScoreTextBox { get; set; }
+        public int Score
+        {
+            get { return _score; }
+            set
+            {
+                ScoreTextBox.Text = value.ToString();
+                FinalScoreTextBox.Text = value.ToString();
+            }
+        } //= this._score;
+        public TextBox ScoreTextBox { set; get; }
+        public int Time
+        {
+            get { return _time; }
+            set
+            {
+                TimeTextBox.Text = value.ToString();
+            }
+        }
+        public TextBox TimeTextBox { set; get; }
+    }
+}
